e83d654fdb7eb137ac94d90d1d4c28fd05f31400
若汐 <liuhw0225@126.com>
2024-09-17 16:49:06 +0800

    # 添加注意力机制

--- models/common.py
+++ models/common.py
@@ -8,7 +8,6 @@ import requests
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from torchvision.ops import DeformConv2d
 from PIL import Image
 from torch.cuda import amp
 
@@ -16,6 +15,7 @@ from utils.datasets import letterbox
 from utils.general import non_max_suppression, make_divisible, scale_coords, increment_path, xyxy2xywh
 from utils.plots import color_list, plot_one_box
 from utils.torch_utils import time_synchronized
+from models.SE import SEAttention
 
 
 ##### basic ####
@@ -2057,97 +2057,43 @@ class ST2CSPC(nn.Module):
 
 
 """
-
+    加入注意力机制transform
 """
 
 
-class MHSA(nn.Module):
-    def __init__(self, n_dims, width=14, height=14, heads=4, pos_emb=False):
-        super(MHSA, self).__init__()
-
-        self.heads = heads
-        self.query = nn.Conv2d(n_dims, n_dims, kernel_size=1)
-        self.key = nn.Conv2d(n_dims, n_dims, kernel_size=1)
-        self.value = nn.Conv2d(n_dims, n_dims, kernel_size=1)
-        self.pos = pos_emb
-        if self.pos:
-            self.rel_h_weight = nn.Parameter(torch.randn([1, heads, (n_dims) // heads, 1, int(height)]),
-                                             requires_grad=True)
-            self.rel_w_weight = nn.Parameter(torch.randn([1, heads, (n_dims) // heads, int(width), 1]),
-                                             requires_grad=True)
-        self.softmax = nn.Softmax(dim=-1)
+class Conv_SE(nn.Module):
+    # Standard convolution
+    def __init__(self, c1, c2, k=1, s=1, p=None, g=1, act=True):  # ch_in, ch_out, kernel, stride, padding, groups
+        super(Conv_SE, self).__init__()
+        self.conv = nn.Conv2d(c1, c2, k, s, autopad(k, p), groups=g, bias=False)
+        self.bn = nn.BatchNorm2d(c2)
+        self.act = nn.SiLU() if act is True else (act if isinstance(act, nn.Module) else nn.Identity())
+        self.att = SEAttention(c2)
 
     def forward(self, x):
-        n_batch, C, width, height = x.size()
-        q = self.query(x).view(n_batch, self.heads, C // self.heads, -1)
-        k = self.key(x).view(n_batch, self.heads, C // self.heads, -1)
-        v = self.value(x).view(n_batch, self.heads, C // self.heads, -1)
-        # print('q shape:{},k shape:{},v shape:{}'.format(q.shape,k.shape,v.shape))  #1,4,64,256
-        content_content = torch.matmul(q.permute(0, 1, 3, 2), k)  # 1,C,h*w,h*w
-        # print("qkT=",content_content.shape)
-        c1, c2, c3, c4 = content_content.size()
-        if self.pos:
-            # print("old content_content shape",content_content.shape) #1,4,256,256
-            content_position = (self.rel_h_weight + self.rel_w_weight).view(1, self.heads, C // self.heads, -1).permute(
-                0, 1, 3, 2)  # 1,4,1024,64
+        return self.att(self.act(self.bn(self.conv(x))))
 
-            content_position = torch.matmul(content_position, q)  # ([1, 4, 1024, 256])
-            content_position = content_position if (
-                    content_content.shape == content_position.shape) else content_position[:, :, :c3, ]
-            assert (content_content.shape == content_position.shape)
-            # print('new pos222-> shape:',content_position.shape)
-            # print('new content222-> shape:',content_content.shape)
-            energy = content_content + content_position
-        else:
-            energy = content_content
-        attention = self.softmax(energy)
-        out = torch.matmul(v, attention.permute(0, 1, 3, 2))  # 1,4,256,64
-        out = out.view(n_batch, C, width, height)
-        return out
+    def fuseforward(self, x):
+        return self.att(self.act(self.conv(x)))
 
 
-class BottleneckTransformer(nn.Module):
-    # Transformer bottleneck
-    # expansion = 1
-
-    def __init__(self, c1, c2, stride=1, heads=4, mhsa=True, resolution=None, expansion=1):
-        super(BottleneckTransformer, self).__init__()
-        c_ = int(c2 * expansion)
-        self.cv1 = Conv(c1, c_, 1, 1)
-        # self.bn1 = nn.BatchNorm2d(c2)
-        if not mhsa:
-            self.cv2 = Conv(c_, c2, 3, 1)
-        else:
-            self.cv2 = nn.ModuleList()
-            self.cv2.append(MHSA(c2, width=int(resolution[0]), height=int(resolution[1]), heads=heads))
-            if stride == 2:
-                self.cv2.append(nn.AvgPool2d(2, 2))
-            self.cv2 = nn.Sequential(*self.cv2)
-        self.shortcut = c1 == c2
-        if stride != 1 or c1 != expansion * c2:
-            self.shortcut = nn.Sequential(
-                nn.Conv2d(c1, expansion * c2, kernel_size=1, stride=stride),
-                nn.BatchNorm2d(expansion * c2)
-            )
-        self.fc1 = nn.Linear(c2, c2)
-
-    def forward(self, x):
-        out = x + self.cv2(self.cv1(x)) if self.shortcut else self.cv2(self.cv1(x))
-        return out
-
-
-class BoT3(nn.Module):
-    # CSP Bottleneck with 3 convolutions
-    def __init__(self, c1, c2, n=1, e=0.5, e2=1, w=20, h=20):  # ch_in, ch_out, number, , expansion,w,h
-        super(BoT3, self).__init__()
-        c_ = int(c2 * e)  # hidden channels
+class SPPCSPC_ATT(nn.Module):
+    # CSP https://github.com/WongKinYiu/CrossStagePartialNetworks
+    def __init__(self, c1, c2, n=1, shortcut=False, g=1, e=0.5, k=(5, 9, 13)):
+        super(SPPCSPC_ATT, self).__init__()
+        c_ = int(2 * c2 * e)  # hidden channels
         self.cv1 = Conv(c1, c_, 1, 1)
         self.cv2 = Conv(c1, c_, 1, 1)
-        self.cv3 = Conv(2 * c_, c2, 1)  # act=FReLU(c2)
-        self.m = nn.Sequential(
-            *[BottleneckTransformer(c_, c_, stride=1, heads=4, mhsa=True, resolution=(w, h), expansion=e2) for _ in
-              range(n)])
-        # self.m = nn.Sequential(*[CrossConv(c_, c_, 3, 1, g, 1.0, shortcut) for _ in range(n)])
+        self.cv3 = Conv(c_, c_, 3, 1)
+        self.cv4 = Conv(c_, c_, 1, 1)
+        self.m = nn.ModuleList([nn.MaxPool2d(kernel_size=x, stride=1, padding=x // 2) for x in k])
+        self.cv5 = Conv(4 * c_, c_, 1, 1)
+        self.cv6 = Conv(c_, c_, 3, 1)
+        self.cv7 = Conv(2 * c_, c2, 1, 1)
+        self.att = SEAttention(c2)
 
     def forward(self, x):
-        return self.cv3(torch.cat((self.m(self.cv1(x)), self.cv2(x)), dim=1))
+        x1 = self.cv4(self.cv3(self.cv1(x)))
+        y1 = self.cv6(self.cv5(torch.cat([x1] + [m(x1) for m in self.m], 1)))
+        y2 = self.cv2(x)
+        return self.att(self.cv7(torch.cat((y1, y2), dim=1)))
